590b85ad75bdcf1e9cd1cd84978d57994e831f87
Unf-Team <unf.team1@gmail.com>
2016-09-29 13:40:53 +0330

    Update self-bot.lua

--- bot/self-bot.lua
+++ bot/self-bot.lua
@@ -51,11 +51,6 @@ function on_binlog_replay_end()
 end
 
 function msg_valid(msg)
-  -- Don't process outgoing messages
-  if msg.out then
-    print('\27[36mNot valid: msg from us\27[39m')
-    return true
-  end
 
   -- Before bot was started
   if msg.date < os.time() - 5 then
@@ -78,10 +73,6 @@ function msg_valid(msg)
     return false
   end
 
-  if msg.from.id == our_id then
-    print('\27[36mNot valid: Msg from our id\27[39m')
-    return true
-  end
 
   if msg.to.type == 'encr_chat' then
     print('\27[36mNot valid: Encrypted chat\27[39m')
